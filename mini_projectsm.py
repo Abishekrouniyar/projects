4d639c40cc1f54f530cd7d99001f744c6de01bd8
Abishekrouniyar <abishekrouniyar47@gmail.com>
2024-10-21 19:47:52 +0530

    Add files via upload

---
+++ mini_projectsm.py
@@ -0,0 +1,53 @@
+menu = {
+   "pasta": 50,
+   "pizza": 80,
+   "noddles": 90,
+   "salad": 60,
+   "burger": 120,
+   "thukpa": 50,
+   "beg momo": 70,
+   "chicken momo": 160,
+   "chicken chilli": 150,
+   "chocolate pestries": 160,
+   "black forest pestries": 120,
+   "cold drink": 55,
+   "cold coffee": 115,
+   "hot coffee": 105,
+}
+
+
+
+
+print("Welcome to KAVYA Bakery & Restaurant")
+
+
+for item_key, item_value in menu.items():
+   print(f"{item_key}: Rs.{item_value}")
+
+
+order_total = 0
+
+
+continue_ordering = True
+
+
+while continue_ordering:
+   item = input("Enter the name of item you want to order =")
+   item = item.lower()
+   if item in menu:
+       order_total += menu[item]
+       print(f"your item {item} has been added to your order")
+   else:
+       print(f"Ordered item {item} is not available yet!")
+   continue_ordering = input("Do you want to add another item? (Yes/No) ") == "Yes"
+
+
+print(f"The total amount of item to pay is {order_total}")
+
+
+
+
+
+
+
+
